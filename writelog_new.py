ad7efdd754a1e4ad4c2d0c03359d1e18ce51ca22
alberto frosi <albert.frosi@gmail.com>
2014-12-19 12:05:04 +0100

    Introdotto re.search

--- writelog_new.py
+++ writelog_new.py
@@ -8,7 +8,6 @@ Created on Dec 12, 2014
 @author: alberto
 '''
 
-
 import re
 from datetime import datetime
 from subprocess import Popen, PIPE, STDOUT
@@ -16,11 +15,12 @@ from pymongo import Connection
 from pymongo.errors import CollectionInvalid
 import pymongo
 
-HOST = 'OEL65'
-LOG_PATH = '/home/oracle/jasperreports-server-cp-5.6.0/apache-tomcat/logs/localhost_access_log.2014-10-07.txt'
+HOST = 'AF-HP'
+LOG_PATH = 'f:\jasperserver.log'
 DB_NAME = 'mydb'
-COLLECTION_NAME = 'jasper1'
-MAX_COLLECTION_SIZE = 5 # in megabytes
+COLLECTION_NAME = 'jasper_new'
+MAX_COLLECTION_SIZE = 5  # in megabytes
+
 
 def main():
     # connect to mongodb
@@ -30,38 +30,36 @@ def main():
     try:
         mongo_coll = mongo_db.create_collection(COLLECTION_NAME,
                                                 capped=True,
-                                                size=MAX_COLLECTION_SIZE*1048576)
+                                                size=MAX_COLLECTION_SIZE * 1048576)
     except CollectionInvalid:
         mongo_coll = mongo_db[COLLECTION_NAME]
 
     # open remote log file
-    cmd = 'tail -f /home/oracle/jasperreports-server-cp-5.6.0/apache-tomcat/logs/localhost_access_log.2014-10-07.txt'
+    cmd = 'tail -f f:\jasperserver.log'
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
     # parse and store data
     while True:
         line = p.stdout.readline()
         data = parse_line(line)
-#         ciccio = line
-##        new_posts = [{"author": data,
-##               "text": "123",
-##               "tags": "abc"}]
-##
-##        mongo_coll.insert(new_posts)
+#        new_posts = [{"loglinetxt": loglinetxt,
+#                      "host": HOST,
+#                      "logpath": LOG_PATH,
+#                      "collection_name": COLLECTION_NAME}]
+#        mongo_coll.insert(new_posts)
+
 def parse_line(line):
     #    m = re.search('controls', line)
-
-    m = re.search( r'(.*)controls(.*?) .*', line, re.M|re.I)
+    #   ('(?<=abc)def', 'abcdef')
+    m = re.search('(?<=coyote)', line)
     if m:
-        print "m.group() : ", m.group()
-        print "m.group(1) : ", m.group(1)
-        print "m.group(2) : ", m.group(2)
+        #        print "m.group() : ", m.group()
+        #        print "m.group(1) : ", m.group(1)
+        #        print "m.group(2) : ", m.group(2)
         print("found a match!")
-
 
     else:
         print ("no match")
         return {}
-
 
 
 if __name__ == '__main__':
